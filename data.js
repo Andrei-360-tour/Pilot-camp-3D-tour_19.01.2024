c8c04852d12bbfd50396ff3ff6cb39a0d1130003
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-01-20 12:22:28 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,818 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pc-main-gate",
+      "name": "PC-Main Gate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -3.053310236569857,
+        "pitch": 0.11773603931185406,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.937660733446421,
+          "pitch": 0.1675395055105806,
+          "rotation": 0,
+          "target": "1-pc-security-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pc-security-area",
+      "name": "PC-Security area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.8491820142430555,
+        "pitch": 0.12166167536561368,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6476013030659757,
+          "pitch": 0.2854304518469313,
+          "rotation": 0,
+          "target": "10-pc--access-road-to-opff"
+        },
+        {
+          "yaw": -0.7982709966406283,
+          "pitch": 0.1783552366165786,
+          "rotation": 0,
+          "target": "2-pc--muster-point"
+        },
+        {
+          "yaw": 2.232279893670251,
+          "pitch": 0.07150818428329409,
+          "rotation": 0,
+          "target": "0-pc-main-gate"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pc--muster-point",
+      "name": "PC- Muster point",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.045506324030237,
+          "pitch": 0.1851810431003713,
+          "rotation": 0,
+          "target": "3-pc-recreation-zone"
+        },
+        {
+          "yaw": 0.39460234376432624,
+          "pitch": 0.0005384795015572763,
+          "rotation": 0,
+          "target": "1-pc-security-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pc-recreation-zone",
+      "name": "PC-Recreation zone",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.31701525868041713,
+        "pitch": 0.22369847469251525,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1311992291676862,
+          "pitch": 0.05954227636467735,
+          "rotation": 6.283185307179586,
+          "target": "5-pc--dinner-room"
+        },
+        {
+          "yaw": 1.3298604749434926,
+          "pitch": 0.08640644527808305,
+          "rotation": 0,
+          "target": "4-pc--smoking-area"
+        },
+        {
+          "yaw": 2.66847396654164,
+          "pitch": 0.28845510425794174,
+          "rotation": 6.283185307179586,
+          "target": "6-pc-living-area"
+        },
+        {
+          "yaw": -2.8251751986926052,
+          "pitch": 0.1468425919858607,
+          "rotation": 1.5707963267948966,
+          "target": "7-pc--laundry-area"
+        },
+        {
+          "yaw": 2.080779905881295,
+          "pitch": 0.15031748103998765,
+          "rotation": 4.71238898038469,
+          "target": "8-pc--sport-area"
+        },
+        {
+          "yaw": -0.19110801399898314,
+          "pitch": 0.018227503904924447,
+          "rotation": 0,
+          "target": "2-pc--muster-point"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-pc--smoking-area",
+      "name": "PC- Smoking area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.334679488396972,
+        "pitch": 0.2851828507776091,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5474894311888985,
+          "pitch": 0.23343508996238072,
+          "rotation": 0,
+          "target": "3-pc-recreation-zone"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pc--dinner-room",
+      "name": "PC- Dinner room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.2912400514034701,
+        "pitch": 0.23033729712790674,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2176537634993583,
+          "pitch": 0.09040876569071266,
+          "rotation": 0,
+          "target": "3-pc-recreation-zone"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0140076386624557,
+          "pitch": 0.16698106041434535,
+          "title": "Title",
+          "text": "Kitchen Room"
+        }
+      ]
+    },
+    {
+      "id": "6-pc-living-area",
+      "name": "PC-Living area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.067340686133602,
+        "pitch": 0.2840401136926598,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20816207137477072,
+          "pitch": 0.19264398314285103,
+          "rotation": 0,
+          "target": "3-pc-recreation-zone"
+        },
+        {
+          "yaw": -1.5099102415228032,
+          "pitch": 0.14592674546118367,
+          "rotation": 0,
+          "target": "7-pc--laundry-area"
+        },
+        {
+          "yaw": 1.638360656328861,
+          "pitch": 0.1507475075954403,
+          "rotation": 0,
+          "target": "8-pc--sport-area"
+        },
+        {
+          "yaw": 3.0194480418377587,
+          "pitch": 0.1401649672190235,
+          "rotation": 0,
+          "target": "9-pc-emergency-gate-living-area"
+        },
+        {
+          "yaw": 0.7469735382490512,
+          "pitch": 0.09847646196927684,
+          "rotation": 7.853981633974483,
+          "target": "4-pc--smoking-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-pc--laundry-area",
+      "name": "PC- Laundry area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.0437083044744675,
+        "pitch": 0.3571326525793026,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.061031416921944,
+          "pitch": 0.12104421439164348,
+          "rotation": 0,
+          "target": "6-pc-living-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pc--sport-area",
+      "name": "PC- Sport area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.9578147644674164,
+        "pitch": 0.4931850618221745,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.933272448781077,
+          "pitch": 0.1984394725085572,
+          "rotation": 0,
+          "target": "6-pc-living-area"
+        },
+        {
+          "yaw": -1.05865167474985,
+          "pitch": 0.3193730136882671,
+          "rotation": 0,
+          "target": "10-pc--access-road-to-opff"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pc-emergency-gate-living-area",
+      "name": "PC-Emergency gate& Living area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.647258736922115,
+          "pitch": 0.11229962381386827,
+          "rotation": 0,
+          "target": "6-pc-living-area"
+        },
+        {
+          "yaw": -0.3362827236876367,
+          "pitch": 0.10808417693096395,
+          "rotation": 0,
+          "target": "11-pc-parking-area-opf"
+        },
+        {
+          "yaw": 0.2783848472610444,
+          "pitch": 0.09205488029196296,
+          "rotation": 0,
+          "target": "14-pc--sound-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-pc--access-road-to-opff",
+      "name": "PC- Access road to OPFF",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.912873812339739,
+          "pitch": -0.02325292404143653,
+          "rotation": 0,
+          "target": "1-pc-security-area"
+        },
+        {
+          "yaw": 0.40987653120429,
+          "pitch": -0.00789053216298008,
+          "rotation": 0,
+          "target": "11-pc-parking-area-opf"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-pc-parking-area-opf",
+      "name": "PC-Parking area OPF",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.4779028824551652,
+        "pitch": 0.29041556363590715,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9468070863108737,
+          "pitch": 0.04064575987409569,
+          "rotation": 4.71238898038469,
+          "target": "9-pc-emergency-gate-living-area"
+        },
+        {
+          "yaw": -2.745272047726626,
+          "pitch": 0.11978662679112873,
+          "rotation": 0,
+          "target": "10-pc--access-road-to-opff"
+        },
+        {
+          "yaw": -0.1438618554068647,
+          "pitch": 0.037382417262943335,
+          "rotation": 0,
+          "target": "12-pc--opf-living-area"
+        },
+        {
+          "yaw": 2.3010307285727816,
+          "pitch": 0.15913754896389953,
+          "rotation": 0,
+          "target": "14-pc--sound-side"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-pc--opf-living-area",
+      "name": "PC- OPF Living area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8056578398286494,
+          "pitch": 0.13350237200404536,
+          "rotation": 7.0685834705770345,
+          "target": "13-pc--opf-guardchack"
+        },
+        {
+          "yaw": -2.1784744029451133,
+          "pitch": 0.12489132686695115,
+          "rotation": 0,
+          "target": "11-pc-parking-area-opf"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-pc--opf-guardchack",
+      "name": "PC- OPF guardchack",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.547952818955551,
+        "pitch": 0.09548147802838969,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.778787891749195,
+          "pitch": 0.16102794994147374,
+          "rotation": 0,
+          "target": "12-pc--opf-living-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-pc--sound-side",
+      "name": "PC- Sound side",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.9581230297149244,
+        "pitch": 0.412824463690491,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2876537775459092,
+          "pitch": 0.17710394520965167,
+          "rotation": 0,
+          "target": "11-pc-parking-area-opf"
+        },
+        {
+          "yaw": 1.7678141666205605,
+          "pitch": 0.07032861176921834,
+          "rotation": 0,
+          "target": "15-pc-southwest-guardchack"
+        },
+        {
+          "yaw": -2.753006481665775,
+          "pitch": 0.1279228431878323,
+          "rotation": 0,
+          "target": "9-pc-emergency-gate-living-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-pc-southwest-guardchack",
+      "name": "PC-Southwest guardchack",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.7492276851317676,
+        "pitch": 0.34041348501703794,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.035361292440701,
+          "pitch": 0.16169040437984705,
+          "rotation": 0,
+          "target": "14-pc--sound-side"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "PILOT CAMP-3D TOUR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
